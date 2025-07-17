200cf61cf11358dc1755b82af9af5b2fb59c4ad2
Cakey01 <alexanderwarren7@gmail.com>
2025-07-17 12:27:58 -0400

    Create play round function

--- rps.js
+++ rps.js
@@ -2,6 +2,7 @@
 let humanScore = 0;
 let computerScore = 0;
 
+
 function getComputerChoice() {
     // make 0-2 random number generator 
     let choice = Math.floor(Math.random() * 3);
@@ -19,7 +20,7 @@ function getComputerChoice() {
     return choice;
 }
 
-function getHumanChoice(answer) {
+function getHumanChoice() {
     // prompt for answer
     let choice = prompt('shoot');
     
@@ -27,4 +28,23 @@ function getHumanChoice(answer) {
     return choice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    // find winner
+    if (humanChoice == computerChoice) {
+        console.log('tie');
+    } else if ((humanChoice == 'rock' && computerChoice == 'scissors') || 
+               (humanChoice == 'paper' && computerChoice == 'rock') || 
+               (humanChoice == 'scissors' && computerChoice == 'rock')) {
+        console.log('you win');
+    } else if ((humanChoice == 'rock' && computerChoice == 'paper') ||
+               (humanChoice == 'paper' && computerChoice == 'scissors') ||
+               (humanChoice == 'scissors' && computerChoice == 'rock')) {
+        console.log('you lose');
+    } 
+    
+
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
