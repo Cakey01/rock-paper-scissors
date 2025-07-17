2afbcf92f41c12d83ec950e81e849a4485f6f29c
Cakey01 <alexanderwarren7@gmail.com>
2025-07-17 11:49:38 -0400

    Create human choice function

--- rps.js
+++ rps.js
@@ -1,5 +1,8 @@
 function getComputerChoice() {
+    // make 0-2 random number generator 
     let choice = Math.floor(Math.random() * 3);
+
+    // make choice numbers into string choices 
     if (choice == 0) {
         choice = 'rock';
     } else if (choice == 1) {
@@ -7,7 +10,17 @@ function getComputerChoice() {
     } else {
         choice = 'scissors';
     }
+
+    // return computer choice string
     return choice;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(answer) {
+    // prompt for answer
+    let choice = prompt('shoot');
+    
+    // convert into lowercase for easier comparison later
+    return choice.toLowerCase();
+}
+
+console.log(getHumanChoice());
