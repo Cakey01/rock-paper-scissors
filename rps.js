4ddaacab433170e261bcba0b2a04122b28a6347b
Cakey01 <alexanderwarren7@gmail.com>
2025-08-20 14:33:52 -0400

    Change playGame function to listen for button click and change console logs to p elements

--- rps.js
+++ rps.js
@@ -1,7 +1,12 @@
 // global variables
 let humanScore = 0;
 let computerScore = 0;
-
+const buttons = document.querySelectorAll("button");
+const result = document.createElement("p");
+const score = document.createElement("p");
+const results = document.querySelector("div")
+results.appendChild(result);
+results.appendChild(score);
 
 function getComputerChoice() {
     // make 0-2 random number generator 
@@ -20,54 +25,43 @@ function getComputerChoice() {
     return choice;
 }
 
-function getHumanChoice() {
-    // prompt for answer
-    let choice = prompt('shoot');
-    
-    // convert into lowercase for easier comparison later
-    return choice.toLowerCase();
-}
-
 function playRound(humanChoice, computerChoice) {
     // find winner
     let win = null;
 
     // tie condititon
     if (humanChoice == computerChoice) {
-        console.log('tie round');
+        result.textContent= 'tie round';
     
     // winning conditions
     } else if ((humanChoice == 'rock' && computerChoice == 'scissors') || 
                (humanChoice == 'paper' && computerChoice == 'rock') || 
-               (humanChoice == 'scissors' && computerChoice == 'rock')) {
-        console.log('you win this round');
+               (humanChoice == 'scissors' && computerChoice == 'paper')) {
+        result.textContent= 'you win this round';
         humanScore++;
     
     // losing conditions
     } else if ((humanChoice == 'rock' && computerChoice == 'paper') ||
                (humanChoice == 'paper' && computerChoice == 'scissors') ||
                (humanChoice == 'scissors' && computerChoice == 'rock')) {
-        console.log('you lose this round');
+        result.textContent= 'you lose this round';
         computerScore++;
     }
-    console.log('you: ' + humanScore, 'computer: ' + computerScore); 
-    
-
+    score.textContent= `you: ${humanScore} computer: ${computerScore}`;
 }
 
-function playGame(round) {
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (humanScore < 5 && computerScore < 5) {
+            let humanChoice = button.id;
+            let computerChoice = getComputerChoice();
+            playRound(humanChoice, computerChoice);
 
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-
-    if (computerScore == humanScore) {
-        console.log("tie game");
-    } else if (computerScore > humanScore) {
-        console.log("you lost the game");
-    } else {
-        console.log('you won the game');
-    }
-}
-
-playGame();
+            if (humanScore === 5) {
+                result.textContent = 'you won the game';
+            } else if (computerScore === 5) {
+                result.textContent = 'you lost the game';
+            }
+        }
+    });
+});
